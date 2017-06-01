32034fd7bb6484fdd39bd408c72274cdc1e19cd6
Bruno Dias <contato@diasbruno.com>
2017-06-01 17:13:37 -0300

    Update TesteIntegracao.java

--- TesteIntegracao.java
+++ TesteIntegracao.java
@@ -1,27 +1,28 @@
-@Test
-public void testPersistePessoa() {
-    Pessoa pessoa = inserirPessoa(criarPessoa(TipoPessoa.FISICA));
+public class PessoaDAOTest extends IntegracaoDeployment {
+    @Test
+    public void testPersistePessoa() {
+        Pessoa pessoa = inserirPessoa(criarPessoa(TipoPessoa.FISICA));
 
-    Assert.assertNotNull(pessoa.getId());
+        Assert.assertNotNull(pessoa.getId());
+    }
+
+    /**
+    * Testa o lançamento de exceção ao se persistir uma pessoa com tipo null
+    */
+    @Test(expectedExceptions = { javax.transaction.RollbackException.class })
+    public void testPersistePessoaTipoNull() {
+        Pessoa pessoa = criarPessoa(TipoPessoa.FISICA);
+        pessoa.setTipo(null);
+
+        inserirPessoa(pessoa);
+    }
+
+    @Test(expectedExceptions = { javax.transaction.RollbackException.class })
+    public void testPersistePessoaDataUltimaEdicaoNull() {
+        Pessoa pessoa = criarPessoa(TipoPessoa.FISICA);
+        pessoa.setUltimaEdicaoNome(null);
+
+        inserirPessoa(pessoa);
+    }
 }
- 
-/**
-* Testa o lançamento de exceção ao se persistir uma pessoa com tipo null
-*/
-@Test(expectedExceptions = { javax.transaction.RollbackException.class })
-public void testPersistePessoaTipoNull() {
-    Pessoa pessoa = criarPessoa(TipoPessoa.FISICA);
-    pessoa.setTipo(null);
-
-    inserirPessoa(pessoa);
-}
-
-@Test(expectedExceptions = { javax.transaction.RollbackException.class })
-public void testPersistePessoaDataUltimaEdicaoNull() {
-    Pessoa pessoa = criarPessoa(TipoPessoa.FISICA);
-    pessoa.setUltimaEdicaoNome(null);
-
-    inserirPessoa(pessoa);
-}
-
 
